2c5b82391bfd9b80bc502c9ed8d36fd79a216c00
Thomas <venturini.thomas95@gmail.com>
2022-02-14 15:30:21 +0100

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+//servirà una funzione che genera 5 numeri casuali
+
+
+//appena generati si avvierà un timer di 30 secondi
+
+//dopo 30 secondi si attiverà un prompt che chiederà all'utente i 5 numeri
+
+//output finale: quanti e quali numeri sono stati indovinati
